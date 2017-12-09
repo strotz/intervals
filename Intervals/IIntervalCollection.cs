7c69fa0868444e77f21a2e28ab6d17fa46dd5595
Alexei Strots <strotz@gmail.com>
2017-12-09 07:31:15 -0800

    initial source code

---
+++ Intervals/IIntervalCollection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Intervals
+{
+	public interface IIntervalCollection
+	{
+		void Add(int start, int stop);
+		void Remove(int start, int stop);
+		IEnumerable<Interval> Items { get; }
+	}
+}
